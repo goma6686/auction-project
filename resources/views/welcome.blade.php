0ad19830a262406697213ee7cc0dfd4590cdb311
Goda Mačiuolytė <goda.maciuolyte@mif.stud.vu.lt>
2022-03-12 05:29:51 +0200

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layout.app')
+
+@section('content')
+<section class="main">
+        <h2 class="main_title">Bid on shid</h2>
+        <p class="main_text">hnnnnngggg aaaaa yyyy ooo eee uuu</p>
+        <br>
+        <p> some list </p>
+        <div id="border">Editable text</div>
+        
+    </head>
+</section>
+@endsection
