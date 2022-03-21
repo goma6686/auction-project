469078fc699641176fc67b5ddb5aa7821db267bb
goma6686 <goma6686@mif.stud.vu.lt>
2022-03-21 12:35:03 +0200

    db changes commit before authentication

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -22,12 +22,16 @@ class ItemSeeder extends Seeder
                 'name' => 'anime mergaite',
                 'description' => 'descr',
                 'condition' => 'new',
+                'min_bid' => '10',
+                'bid_sum' => '100',
                 'cover' => 'https://otakukart.com/wp-content/uploads/2021/12/zero-two.jpg'
             ],
             [
                 'name' => 'anime mergaite2',
                 'description' => 'descr',
-                'condition' => 'new',
+                'condition' => 'used',
+                'min_bid' => '120',
+                'bid_sum' => '200',
                 'cover' => 'https://static.boredpanda.com/blog/wp-content/uploads/2019/03/image-5c90517716638__700.jpg'
             ],
         ]);
