2eb14ca86d4d35a7af81834be2476cb3ddbbd7f6
goma6686 <goda.maciuolyte@mif.stud.vu.lt>
2022-06-08 20:33:42 +0300

    deleted vue, changed nav

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,21 +1,21 @@
+<nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="{{ route('dashboard') }}">
+            {{ config('app.name', 'Auction') }}
+        </a>
 
-<nav class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
-        <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
-            <li class="nav-item active">
-                <a class="nav-link px-2 text-secondary" href="{{ route('dashboard') }}">Auction</a>
-            </li>
-        </ul>
-        <div class="text-end">
-            @guest
+        <div class="text-end" id="navbarSupportedContent">
+                <!-- Authentication Links -->
+                @guest
                 @if (Route::has('login'))
-                    <a class="btn btn-outline-light me-2" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    <a class="btn btn-outline-dark me-2" href="{{ route('login') }}">{{ __('Login') }}</a>
                 @endif
 
                 @if (Route::has('register'))
-                    <a class="btn btn-outline-light me-2" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    <a class="btn btn-outline-dark me-2" href="{{ route('register') }}">{{ __('Register') }}</a>
                 @endif
             @else
-                    <a id="navbarDropdown" class="btn btn-outline-light me-2 dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                    <a id="navbarDropdown" class="btn btn-outline-dark me-2 dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                         {{ Auth::user()->name }}
                     </a>
 
@@ -33,4 +33,5 @@
                     </div>
                 @endguest
         </div>
+    </div>
 </nav>
